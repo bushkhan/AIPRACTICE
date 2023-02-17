a4c76c3ac8aa6fadfbaa2b24197655882f3f647c
Bushra Khan <bushrakhan2212002@gmail.com>
2023-02-17 16:00:09 +0530

    object tracking 2

--- Day6/object_tracking.py
+++ Day6/object_tracking.py
@@ -5,3 +5,12 @@ blackLower=(0,0,0)
 blackUpper=(9360,255,50)
 
 camera = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+while True:
+    _,frame = camera.read()
+    frame = imutils.resize(frame,width=600)
+    blurred = cv2.GaussianBlur(frame, (11,11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+
+
