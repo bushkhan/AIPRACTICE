6d5638b325c8f80c34c9761ebcae71afc4295347
Bushra Khan <bushrakhan2212002@gmail.com>
2023-02-17 16:14:08 +0530

    object tracking 3

--- Day6/object_tracking.py
+++ Day6/object_tracking.py
@@ -2,7 +2,7 @@ import cv2
 import imutils
 
 blackLower=(0,0,0)
-blackUpper=(9360,255,50)
+blackUpper=(360,255,50)
 
 camera = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 
@@ -11,6 +11,16 @@ while True:
     frame = imutils.resize(frame,width=600)
     blurred = cv2.GaussianBlur(frame, (11,11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsv,blackLower,blackUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+
+
+
 
 
 
